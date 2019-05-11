707e50a035b1158c5eabb2f67129553211c65bf6
VioletMiku <livuanh97@gmail.com>
2019-05-11 19:16:44 +0700

    a bit worry about cancel between customer and fixer

--- app/src/main/java/com/keyfixer/partner/Services/OurFirebaseMessaging.java
+++ app/src/main/java/com/keyfixer/partner/Services/OurFirebaseMessaging.java
@@ -1,6 +1,10 @@
 package com.keyfixer.partner.Services;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -19,17 +23,35 @@ public class OurFirebaseMessaging extends FirebaseMessagingService {
         //so we're going to convert message to Latng
         if (remoteMessage.getData() != null) {
             Map<String, String> data = remoteMessage.getData();
-            String customer = data.get("customer");
-            String lat = data.get("lat");
-            String lng = data.get("lng");
-            Common.service_chose = data.get("service");
 
-            Intent intent = new Intent(getBaseContext() , CustomerCallActivity.class);
-            intent.putExtra("lat" , lat);
-            intent.putExtra("lng" , lng);
-            intent.putExtra("customer" , customer);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
+            String customerRequest = data.get("customer");
+            try{
+                if (customerRequest.equals("Cancel")){
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(OurFirebaseMessaging.this, "Khách hàng đã hủy", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else{
+
+                    final String message = data.get("message");
+                    String lat = data.get("lat");
+                    String lng = data.get("lng");
+                    Common.service_chose = data.get("service");
+
+                    Intent intent = new Intent(getBaseContext() , CustomerCallActivity.class);
+                    intent.putExtra("lat" , lat);
+                    intent.putExtra("lng" , lng);
+                    intent.putExtra("customer" , customerRequest);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }catch (Exception ex){
+                Log.e("Warning","Something wrong");
+                Log.e("Exception", "" + ex.getCause());
+            }
         }
     }
 }
